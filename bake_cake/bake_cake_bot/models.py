974023dfccd9ade04f2feb8b530c2d767d7ad14c
Yulia51188 <sun51188@gmail.com>
2021-10-20 13:11:54 +0300

    Add Client to models

--- bake_cake/bake_cake_bot/models.py
+++ bake_cake/bake_cake_bot/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    first_name = models.CharField('Имя', max_length=100, db_index=True)
+    last_name = models.CharField('Фамилия', max_length=100, db_index=True,
+                                 blank=True)
+    phone = models.CharField('Телефон', max_length=15, db_index=True)
+    address = models.CharField('Адрес', max_length=256, blank=True, default='')
+    pd_proccessing_consent = models.NullBooleanField(
+        'Согласие на обработку ПД')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.phone})'
