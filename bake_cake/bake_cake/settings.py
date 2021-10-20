e41045e2fe626793cbbe3fb0c9c4f6c78bb078b1
Yulia51188 <sun51188@gmail.com>
2021-10-20 11:27:51 +0300

    Add custom command runbot

--- bake_cake/bake_cake/settings.py
+++ bake_cake/bake_cake/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('SECRET_KEY')
-SECRET_KEY = env.str('TG_TOKEN')
+TG_TOKEN = env.str('TG_TOKEN')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=True)
