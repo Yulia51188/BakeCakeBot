cd8fe96b89d0fc9bf105fb4c83af569ba69614d4
Yulia51188 <sun51188@gmail.com>
2021-10-20 11:20:11 +0300

    Add app in settings

--- bake_cake/bake_cake/settings.py
+++ bake_cake/bake_cake/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'bake_cake_bot.apps.BakeCakeBotConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
