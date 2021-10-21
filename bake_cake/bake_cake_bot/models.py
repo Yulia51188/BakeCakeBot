03601e7bea5fe53fc2a2d28f9ae16b9c6db719f3
Yulia51188 <sun51188@gmail.com>
2021-10-21 14:26:48 +0300

    Fix code formatting

--- bake_cake/bake_cake_bot/models.py
+++ bake_cake/bake_cake_bot/models.py
@@ -2,17 +2,33 @@ from django.db import models
 
 
 class Client(models.Model):
-    tg_chat_id = models.PositiveIntegerField('ID чата Телеграм', unique=True,
-        db_index=True)
+    tg_chat_id = models.PositiveIntegerField(
+        'ID чата Телеграм',
+        unique=True,
+        db_index=True
+    )
     first_name = models.CharField('Имя', max_length=100, db_index=True)
 
-    last_name = models.CharField('Фамилия', max_length=100, db_index=True,
-                                 blank=True, default='')
-    phone = models.CharField('Телефон', max_length=15, db_index=True,
-        blank=True, default='')
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=100,
+        db_index=True,
+        blank=True,
+        default=''
+    )
+    phone = models.CharField(
+        'Телефон',
+        max_length=15,
+        db_index=True,
+        blank=True,
+        default=''
+    )
     address = models.CharField('Адрес', max_length=256, blank=True, default='')
     pd_proccessing_consent = models.BooleanField(
-        'Согласие на обработку ПД', null=True, blank=True)
+        'Согласие на обработку ПД',
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} ({self.tg_chat_id})'
@@ -29,8 +45,13 @@ class Category(models.Model):
 class Option(models.Model):
     name = models.CharField('Название', max_length=100, db_index=True)
     price = models.IntegerField('Цена', db_index=True)
-    category = models.ForeignKey('Category', verbose_name='Категория',
-        on_delete=models.CASCADE, db_index=True, related_name='options')
+    category = models.ForeignKey(
+        'Category',
+        verbose_name='Категория',
+        on_delete=models.CASCADE,
+        db_index=True,
+        related_name='options'
+    )
 
     def __str__(self):
         return f'{self.category} {self.name}'
