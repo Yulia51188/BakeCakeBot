4abb4fd929f443c38b3a3ff10659a3668b20ac99
Anton Rozhdestvensky <makefrontend@ya.ru>
2021-10-21 03:33:04 +0300

    первичная структура бота

--- bake_cake/bake_cake_bot/management/commands/runbot.py
+++ bake_cake/bake_cake_bot/management/commands/runbot.py
@@ -15,7 +15,7 @@ bot.
 
 import logging
 
-from telegram import Update, ForceReply
+from telegram import Update, ForceReply, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
 
 from django.core.management.base import BaseCommand
@@ -27,6 +27,103 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
+
+NUMBER_LEVELS, FORM, ADDRESS = range(3)
+
+
+# Dialogue keyboards
+def main_keyboard(chat_id):
+    if not Profile.objects.get(user_id__contains=chat_id):
+        markup = ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text='Регистрация'),
+                ]
+            ],
+            resize_keyboard=True
+        )
+        return markup
+    else:
+        markup = ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text='Собрать торт'),
+                    KeyboardButton(text='Ваши заказы')
+                ]
+            ],
+            resize_keyboard=True
+        )
+        return markup
+
+
+def registration_keyboard():
+    markup = ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                KeyboardButton(text='Согласиться'),
+                KeyboardButton(text='Отказаться'),
+            ]
+        ],
+        resize_keyboard=True
+    )
+    return markup
+
+
+def order_cake_keyboard():
+    markup = ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                KeyboardButton(text='Заказать торт'),
+                KeyboardButton(text='В главное меню'),
+            ]
+        ],
+        resize_keyboard=True
+    )
+    return markup
+
+
+# Cake composition
+def levels_keyboard():
+    markup=InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton('1 уровень (+400р)', callback_data='+ 400р'),
+                InlineKeyboardButton('2 уровня (+750р)', callback_data='+ 750р'),
+                InlineKeyboardButton('3 уровня (+1100р)', callback_data='+ 1100р'),
+            ]
+        ]
+    )
+    return markup
+
+
+def form_keyboard():
+    markup=InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton('Квадрат (+600)', callback_data='+ 600р'),
+                InlineKeyboardButton('Круг (+400)', callback_data='+ 400р'),
+                InlineKeyboardButton('Прямоугольник (+1000)', callback_data='+ 1000р'),
+            ]
+        ]
+    )
+    return markup
+
+
+def topping_keyboard():
+    markup=InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton('Без топпинга (+0)', callback_data='+ 0р'),
+                InlineKeyboardButton('Белый соус (+200)', callback_data='+ 200р'),
+                InlineKeyboardButton('Карамельный сироп (+180)', callback_data='+ 180р'),
+                InlineKeyboardButton('Клубничный сироп (+300)', callback_data='+ 300р'),
+                InlineKeyboardButton('Черничный сироп (+350)', callback_data='+ 350р'),
+                InlineKeyboardButton('Молочный шоколад (+200)', callback_data='+ 200р'),
+            ]
+        ]
+    )
+    return markup
 
 
 # Define a few command handlers. These usually take the two arguments update and
@@ -50,15 +147,58 @@ def echo(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(update.message.text)
 
 
-def cancel_handler(update, context):
-    if update.message.text == 'Отменить заказ':
+# user registration
+def registration_handler(update, context):
+    chat_id = update.effective_chat.id
+    if update.message.text == 'Регистрация':
+        # Отправка файла из папки files
+        file_handler(chat_id)
         context.bot.send_message(
-            chat_id = update.effective_chat.id,
-            text = f'Вы отменили размещение заказа :(',
-            reply_markup = main_keyboard(chat_id)
+            chat_id=chat_id,
+            text=f'Ознакомьтесь с политикой по обработке персональных данных.',
+            reply_markup=registration_keyboard()
         )
-        return ConversationHandler.END
 
+    if update.message.text == 'Согласиться':
+        context.bot.send_message(
+            chat_id=chat_id,
+            text=f'Вы находитесь в главном меню. Выберите нужный пункт.',
+            reply_markup=order_cake_keyboard()
+        )
+    # <- Добавить функцию записи user_registration_db()
+    
+    if update.message.text == 'Отказаться':
+            context.bot.send_message(
+            chat_id=chat_id,
+            text=f'К сожалению, мы не сможем приготовить Вам торт :((.',
+            reply_markup=main_keyboard(chat_id)
+        )
+
+
+def file_handler(context, chat_id):
+    with open('../../files/personal_data_policy.pdf', 'rb') as file:
+        return context.bot.send_document(chat_id, file)
+
+
+def user_registration_db():
+    pass
+
+
+# Place an order or redirect the main menu
+def order_cake_handler(update, context):
+    pass
+
+
+def query_handler(update: Update, context: CallbackContext):
+    pass
+
+
+def cancel_handler(update, context):
+    user = update.effective_user
+    update.message.reply_text(
+        f'Очень жаль, что вы отменили заказ :((. Возвращайтесь!'
+    )
+    return ConversationHandler.END
 
 
 def run_bot(tg_token) -> None:
@@ -72,10 +212,32 @@ def run_bot(tg_token) -> None:
     # Dialogue system for ordering a cake
     conv_handler = ConversationHandler(
         entry_points=[
-
+            MessageHandler(Filters.text, order_cake),
         ],
         states={
-
+            NUMBER_LEVELS: [
+                MessageHandler(
+                    Filters.regex('^1 уровень&'),
+                    callback,
+                    pass_user_data=True
+                ),
+                MessageHandler(
+                    Filters.regex('^2 уровня&'),
+                    callback,
+                    pass_user_data=True
+                ),
+                MessageHandler(
+                    Filters.regex('^3 уровня&'),
+                    callback,
+                    pass_user_data=True
+                ),
+            ],
+            FORM: [
+                MessageHandler(Filters.text, callback, pass_user_data=True)
+            ],
+            ADDRESS: [
+                MessageHandler(Filters.text, callback, pass_user_data=True)
+            ]
         },
         fallbacks=[
             MessageHandler(Filters.text, cancel_handler)
@@ -90,7 +252,29 @@ def run_bot(tg_token) -> None:
     # on non command i.e message - echo the message on Telegram
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
-    # 
+    # user registration
+    dispatcher.add_handler(
+        MessageHandler(
+            Filters.regex('^Регистрация&'),
+            callback=registration_handler
+        )
+    )
+
+    dispatcher.add_handler(
+        MessageHandler(
+            Filters.regex('^Согласиться&'),
+            callback=registration_handler
+        )
+    )
+
+    dispatcher.add_handler(
+        MessageHandler(
+            Filters.regex('^Отказаться&'),
+            callback=registration_handler
+        )
+    )
+
+    # Dialogue system for ordering a cake
     dispatcher.add_handler(conv_handler)
 
     # Start the Bot
